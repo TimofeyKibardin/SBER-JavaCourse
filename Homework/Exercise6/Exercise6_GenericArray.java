f651096ff43cf3eb44c546591a6a621dae9ab990
TimofeyKibardin <timoshka.kartoshka99@gmail.com>
2021-04-09 12:37:47 +0300

    Updated Exercise6

--- Homework/Exercise6/Exercise6_GenericArray.java
+++ Homework/Exercise6/Exercise6_GenericArray.java
@@ -2,127 +2,89 @@ package Homework.Exercise6;
 
 import java.util.*;
 
-class Gen <T extends Number> {
-    ArrayList<T> obj;
-
-    //Передаём конструктору ссылку на объект числовой коллекции
-    Gen(ArrayList<T> o) {
-        obj = o;
-    }
-
-    int getSize(ArrayList<T> obj) {
-        return obj.size();
-    }
-
-    Object get(int index) {
-        return obj.get(index);
-    }
-
-    void set(int index, T elem) {
-        obj.set(index, elem);
-    }
-
-    void delete(int index) {
-        obj.remove(index);
-    }
-
-    /**
-     * Не понимаю, как реализовать этот метод
-     */
-    /* void sort() {
-        Collections.sort(obj);
-    } */
-
-    void toArray() {
-        obj.toArray();
-    }
-
-    boolean equals(List o) {
-        return obj.equals(o);
-    }
-
-    void iterator() {
-        Iterator<T> it = obj.iterator();
-        System.out.println("Массив через итератор");
-        while (it.hasNext()) {
-            System.out.print(it.next() + " ");
-        }
-        System.out.println();
-    }
-
-    void add(int index, T element) {
-        obj.add(index, element);
-        System.out.println("Вы добавили элемент " + element + " по индексу " + index);
-    }
-
-    void addAll(int index, Collection<T> c) {
-        obj.addAll(index, c);
-        System.out.println("Коллекция теперь выглядит так: " + obj.toString());
-    }
-
-    int indexOf(Object o) {
-        return obj.indexOf(o);
-    }
-
-    int lastIndexOf(Object o) {
-        return obj.lastIndexOf(o);
-    }
-
-    Collection<T> subList(int fromIndex, int toIndex) {
-        Collection<T> objSub = obj.subList(fromIndex, toIndex);
-        return objSub;
-    }
-}
-
 public class Exercise6_GenericArray {
     public static void main(String[] args) {
 
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(i, (int) (Math.random() * 100));
+        MyArrayList<Integer> ob = new MyArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            ob.add(i);
         }
-        Gen<Integer> ob = new Gen<>(list);
+        System.out.print("Массив: ");
+        for (int i = 0; i < ob.size(); i++) {
+            System.out.print(ob.get(i) + " ");
+        }
+        System.out.println();
 
-        //1 getSize
-        System.out.println("getSize: " + ob.getSize(list));
+        //1 size()
+        System.out.println("getSize: " + ob.size());
 
-        //2 get
+        //2 get()
         System.out.println("get: " + ob.get(0));
 
-        //3 set
-        ob.set(0, 11);
-        System.out.println("set: " + ob.get(0));
+        //3 set()
+        ob.set(0, 1000);
+        System.out.println("set 0: " + ob.get(0));
 
-        //4 delete
-        ob.delete(0);
+        //4 delete()
+        ob.delete(12);
+        System.out.print("Массив: ");
+        for (int i = 0; i < ob.size(); i++) {
+            System.out.print(ob.get(i) + " ");
+        }
+        System.out.println();
 
-        /* 5 sort
-        ob.sort();
-        System.out.println("sorted: " + ob.toString()); */
+        //5 indexOf()
+        System.out.println("indexOf 3: " + ob.indexOf(3));
 
-        //6 toArray
-        ob.toArray();
+        //6 lastIndexOf()
+        System.out.println("lastIndexOf 1000: " + ob.lastIndexOf(7));
 
-        //7 equals
-        ArrayList<Integer> list2 = (ArrayList)list.clone();
-        System.out.println(ob.equals(list2));
+        //7 toArray()
+        ob.toArray(); //Хотя здесь и так нет коллекции
 
-        //8 iterator
-        ob.iterator();
+        //8 clear()
+        //ob.clear();
 
         //9 add
-        ob.add(0, 1000);
+        ob.add(5000);
+        ob.add(4, 1000);
 
-        //10 addAll
-        ob.addAll(0, list2);
+        //10 addAll()
+        ArrayList<Integer> ob2 = new ArrayList<>();
+        for (int i = 0; i < ob.size(); i++) {
+            ob2.add((int)(Math.random() * -100));
+        }
+        ob.addAll(1, ob2);
+        System.out.print("Массив ob + ob2: ");
+        for (int i = 0; i < ob.size(); i++) {
+            System.out.print(ob.get(i) + " ");
+        }
+        System.out.println();
 
-        //11 indexOf
-        System.out.println(ob.indexOf(1000));
+        //11 equals()
+        System.out.println("equals: " + ob.equals(ob2));
 
-        //12 lastIndexOf
-        System.out.println(ob.lastIndexOf(1000));
+        //12 sublist()
+        MyArrayList<Integer> ob3 = ob.subList(10, 15);
+        System.out.print("sublist: ");
+        for (int i = 0; i < ob3.size(); i++) {
+            System.out.print(ob3.get(i) + " ");
+        }
+        System.out.println();
 
-        //13 sublist
-        System.out.println(ob.subList(0, 4).toString());
+        //13 iterator()
+        Iterator<Integer> it = ob3.iterator();
+        System.out.print("iterator: ");
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+
+        //14 sort()
+        /* ob.sort();
+        System.out.print("Массив ob после sort(): ");
+        for (int i = 0; i < ob.size(); i++) {
+            System.out.print(ob.get(i) + " ");
+        }
+        System.out.println(); */
     }
 }
---
+++ Homework/Exercise6/MyArrayList.java
@@ -0,0 +1,203 @@
+package Homework.Exercise6;
+
+import java.util.*;
+
+class MyArrayList <T> {
+    private final int INIT_SIZE = 10;
+    private final int CUT_RATE = 2;
+    private Object[] obj = new Object[INIT_SIZE];
+    private int pointer; //размер массива, получение через size()
+
+
+    /*Вспомогательный метод для масштабирования*/
+    private void resize(int newLength) {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(obj, 0, newArray, 0, pointer);
+        obj = newArray;
+    }
+
+    /*Добавить элемент*/
+    void add(T value) {
+        if (pointer == obj.length - 1) {
+            resize(obj.length * 2);
+        }
+        obj[pointer++] = value;
+    }
+
+    /*Добавить элемент по индексу*/
+    void add(int index, T value) {
+        if (index < 0) return;
+        if (pointer == obj.length - 1) {
+            resize(obj.length * 2);
+        }
+
+        if (index > pointer) {
+            index = pointer;
+        }
+
+        for (int i = pointer; i >= index; i--) {
+            obj[i + 1] = obj[i];
+        }
+        obj[index] = value;
+        pointer++;
+    }
+
+    /*Добавить всю коллекцию в конец массива*/
+    boolean addAll(Collection<T> c) {
+        if (c == null) {
+            return false;
+        }
+        if (c.isEmpty()) {
+            return false;
+        }
+        for (T value : c) {
+            add(value);
+        }
+        return true;
+    }
+    /*Добавить всю коллекцию в массив начиная с индекса*/
+    boolean addAll(int index, Collection<T> c) {
+        if (c == null) {
+            return false;
+        }
+        if (c.isEmpty() || index < 0) {
+            return false;
+        }
+        if (index > pointer) {
+            index = pointer;
+        }
+        for (T value : c) {
+            add(index++, value);
+        }
+        return true;
+    }
+
+    /*Размер массива*/
+    int size() {
+        return pointer;
+    }
+
+    /*Получить элемент по индексу*/
+    T get(int index) {
+        return (T) obj[index];
+    }
+
+    /*Установить элемент по индексу*/
+    void set(int index, T value) {
+        obj[index] = value;
+    }
+
+    /*Удалить элемент по индексу*/
+    void delete(int index) {
+        for (int i = index; i < pointer; i++) {
+            obj[i] = obj[i + 1];
+        }
+        obj[pointer] = null;
+        pointer--;
+        if (obj.length > INIT_SIZE && pointer < obj.length / CUT_RATE) {
+            resize(obj.length / 2);
+        }
+    }
+
+    /*Найти элемент в массиве по индексу*/
+    int indexOf(T value) {
+        int result = -1;
+        for (int i = 0; i < obj.length; i++) {
+            if (obj[i].equals(value)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /*Получение индекса последнего вхождения элемента*/
+    int lastIndexOf(T value) {
+        int lastIndex = -1;
+        if (value == null) {
+            for (int i = 0; i < pointer; i++) {
+                if (obj[i] == null) {
+                    lastIndex = i;
+                }
+            }
+            return lastIndex;
+        } else {
+            for (int i = 0; i < pointer; i++) {
+                if (value.equals(obj[i])) {
+                    lastIndex = i;
+                }
+            }
+            return lastIndex;
+        }
+    }
+
+    /*Преобразовать коллекцию в массив*/
+    Object[] toArray() {
+        Object[] newArray = new Object[obj.length];
+        System.arraycopy(obj, 0, newArray, 0, size());
+        return newArray;
+    }
+
+    /*Очистить массив*/
+    void clear() {
+        for (int i = 0; i < pointer; i++) {
+            obj[i] = null;
+        }
+        pointer = 0;
+    }
+
+    /*Проверить равенство двух массивов по хэшкоду*/
+    boolean equals(Collection<T> ob2) {
+        if (ob2 == null) return false;
+        if (Arrays.hashCode(obj) == ob2.hashCode()) {
+            return true;
+        } else return false;
+    }
+
+    /*Создать саблист*/
+    MyArrayList<T> subList(int fromIndex, int toIndex) {
+        if (fromIndex > toIndex) {
+            int temp = fromIndex;
+            fromIndex = toIndex;
+            toIndex = temp;
+        }
+
+        if ((fromIndex < 0) || (toIndex > pointer)) {
+            return null;
+        }
+
+        MyArrayList<T> newSubList = new MyArrayList<>();
+        for (int i = fromIndex; i < toIndex; i++) {
+            newSubList.add((T) obj[i]);
+        }
+        return newSubList;
+    }
+
+    /*Итератор*/
+    Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < pointer && obj[currentIndex] != null;
+            }
+
+            @Override
+            public T next() throws IndexOutOfBoundsException {
+                return (T) obj[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    /*Сортировка*/
+    /* void sort() {
+        Arrays.sort(obj, Comparator.comparing(Object::toString));
+    } */
+}
